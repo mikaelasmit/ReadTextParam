068827c2f70fbaac60dc736e5e01e953ba339784
mikaelasmit <mikaela.smit@gmail.com>
2015-10-28 15:15:30 +0000

    Now reading several lines until ';'

--- CparamReader.cpp
+++ CparamReader.cpp
@@ -24,16 +24,16 @@ using namespace std;
 // Class Constructor
 CParamReader::CParamReader()
 {
-	paramBuffer = new char[BUFFER_SIZE];
-	filePathString = NULL;
+    paramBuffer = new char[BUFFER_SIZE];
+    filePathString = NULL;
 }
 
 // Class Destructor
 CParamReader::~CParamReader()
 {
-	delete[] paramBuffer;
-	if(filePathString!=NULL)
-		delete[] filePathString;
+    delete[] paramBuffer;
+    if(filePathString!=NULL)
+        delete[] filePathString;
 }
 
 
@@ -41,132 +41,143 @@ CParamReader::~CParamReader()
 // unattaches previous file if necessary.
 bool CParamReader::setNewFileName(char* filePath)
 {
-	// If there's a file attached, remove it.
-	if(paramFileStream.is_open())
-		paramFileStream.close();
-
-	paramFileStream.open(filePath);
-
-	if(!paramFileStream.is_open())
-		return false;
-
-	paramFileStream.close(); // Just checking, close it
-
-	filePathString = new char[strlen(filePath)+1];
-	strcpy(filePathString, filePath);
-
-	return true;
+    // If there's a file attached, remove it.
+    if(paramFileStream.is_open())
+        paramFileStream.close();
+    
+    paramFileStream.open(filePath);
+    
+    if(!paramFileStream.is_open())
+        return false;
+    
+    paramFileStream.close(); // Just checking, close it
+    
+    filePathString = new char[strlen(filePath)+1];
+    strcpy(filePathString, filePath);
+    
+    return true;
 }
 
 // Return a string containing parameter data or NULL and also the length of the vector.
 char* CParamReader::getParamString(const char* paramName, int& nElements)
 {
-	// Do we have a file attached?
-	paramFileStream.open(filePathString);
-
-	if(!paramFileStream.is_open())
-		return NULL;
-
-
-	// Declare variables needed 
-	int paramLength = strlen(paramName);
-	bool found = false;
-	char* paramString = NULL;
-	char* paramContainer = NULL;
-	char charContainer[1000];
-	char* token;
-	int count;
-	int nullTerminal;
-
-
-	// Run through the lines of the file for the parameter
-	while(!paramFileStream.eof() && !found)
-	{
-		paramFileStream.getline(paramBuffer,BUFFER_SIZE-1);
-		token = strtok(paramBuffer,"\t");		// This reads the first bit until 'tab' "\t"
-		
-		// Anything on this line?
-		if(token!=NULL)
-		{
-			// Line doesn't begin with # and matches param name?
-			if(token[0]!='#'&& token[0]!=';' && strcmp(paramName,token)==0)
-			{
-				int count=0;
-				cout << "Token: " << token << endl;
-					
-
-				while (token[0]!=';')
-				{
-
-					// Lets get the next line and first line of data
-					paramFileStream.getline(paramBuffer,BUFFER_SIZE-1);
-					cout << "Token: " << token << endl;
-					paramString = strtok(token,"\t");
-					cout << "paramString: " << paramString << endl;
-
-					if (token[0]!=';'){
-					
-
-						// 24/04/15: Remove any extra spaces from the end of the string (as long as the string isn't zero length).
-						nullTerminal = strlen(paramString);		// This gets the length of the char string
-						cout << "nullTerminal: " << nullTerminal << endl;
-						while(paramString[nullTerminal-1]==' ')  // This removes all extra spaces 
-						{
-							nullTerminal--;							// means minus minus (e.g. from 3 to 2)
-							cout << "we removed the spaces " << endl;
-							cout << "nullTerminal: " << nullTerminal << endl;
-						}
-
-						paramString[nullTerminal] = '\0';		// ends the string in a specific way
-		
-						
-
-
-						// Lets add this data to the paramContainer
-						if (count==0)
-						{
-							strcpy(charContainer, paramString);
-						}
-						else if (count>0)
-						{
-							strcat(charContainer, " ");
-							strcat(charContainer, paramString);
-						}
-						count++;
-			
-
-						cout << "Round 2: " << endl;
-						cout << "ParamStr: \t" << paramString << endl;
-						cout << "Container: \t" << charContainer << endl; 
-
-					}
-					else
-					{
-						found = true;
-						paramContainer=charContainer;
-						cout << "FINAL: " << charContainer << endl;
-					}
-
-				}
-				found = true;
-			}
-		}
-	}
-
-
-	// count the spaces. Single space separates values so length of vector is #spaces + 1.
-	nullTerminal = strlen(paramContainer);		// This gets the length of the char string
-	int count2 = 0;
-	for(int j=0;j<nullTerminal;j++)
-		if(paramContainer[j]==' ') count2++;
-
-	//nElements = count+ 1;
-	nElements=count2 + 1;
-	cout << "NElements: " << nElements << endl;
-
-	paramFileStream.close();
-
-	return paramContainer;
+    // Do we have a file attached?
+    paramFileStream.open(filePathString);
+    
+    if(!paramFileStream.is_open())
+        return NULL;
+    
+    
+    // Declare variables needed
+    int paramLength = strlen(paramName);
+    bool found = false;
+    string paramString;
+    string paramContainer;
+    //char charContainer[1000];
+    char* token;
+    int count;
+    int nullTerminal;
+    
+    
+    // Run through the lines of the file for the parameter
+    while(!paramFileStream.eof() && !found)
+    {
+        paramFileStream.getline(paramBuffer,BUFFER_SIZE-1);
+        token = strtok(paramBuffer,"\t");		// This reads the first bit until 'tab' "\t"
+        
+        // Anything on this line?
+        if(token!=NULL)
+        {
+            // Line doesn't begin with # and matches param name?
+            if(token[0]!='#'&& token[0]!=';' && strcmp(paramName,token)==0)
+            {
+                int count=0;
+                cout << "Token: " << token << endl;
+                
+                
+                while (token[0]!=';')
+                {
+                    
+                    // Lets get the next line and first line of data
+                    paramFileStream.getline(paramBuffer,BUFFER_SIZE-1);
+                    cout << "Token: " << token << endl;
+                    paramString = strtok(token,"\t");
+                    cout << "paramString: " << paramString << endl;
+                    
+                    if (token[0]!=';'){
+                        
+                        
+                        // 24/04/15: Remove any extra spaces from the end of the string (as long as the string isn't zero length).
+                        nullTerminal = strlen(token);		// This gets the length of the char string
+                        cout << "nullTerminal: " << nullTerminal << endl;
+                        while(token[nullTerminal-1]==' ')  // This removes all extra spaces
+                        {
+                            nullTerminal--;							// means minus minus (e.g. from 3 to 2)
+                            cout << "we removed the spaces " << endl;
+                            cout << "nullTerminal: " << nullTerminal << endl;
+                        }
+                        
+                        token[nullTerminal] = '\0';		// ends the string in a specific way
+                        
+                        
+                        
+                        
+                        // Lets add this data to the paramContainer
+                        if (count==0)
+                        {
+                            //strcpy(charContainer, paramString);
+                            paramContainer=token;
+                            cout << "ParamContainer: " << paramContainer << endl;
+                        }
+                        else if (count>0)
+                        {
+                            
+                            paramString=token;
+                            paramContainer.append(" ");
+                            paramContainer.append(paramString);
+                            cout << "ParamString: " << paramString << " and paramContainer: " << paramContainer << endl;
+                            
+                            //strcat(charContainer, " ");
+                            //strcat(charContainer, paramString);
+                        }
+                        count++;
+                        
+                        
+                        cout << "Round 2: " << endl;
+                        cout << "ParamStr: \t" << paramString << endl;
+                        cout << "Container: \t" << paramContainer << endl;
+                        
+                    }
+                    else
+                    {
+                        found = true;
+                        paramContainer=paramContainer;
+;
+                        cout << "FINAL: " <<paramContainer << endl;
+                    }
+                    
+                }
+                found = true;
+            }
+        }
+    }
+    
+    
+    // count the spaces. Single space separates values so length of vector is #spaces + 1.
+    char* charContainer = new char[paramContainer.length() +1];
+    strcpy(charContainer, paramContainer.c_str());
+    nullTerminal = strlen(charContainer);		// This gets the length of the char string
+    int count2 = 0;
+    for(int j=0;j<nullTerminal;j++)
+        if(paramContainer[j]==' ') count2++;
+    
+    //nElements = count+ 1;
+    nElements=count2 + 1;
+    cout << "NElements: " << nElements << endl;
+    
+    paramFileStream.close();
+    
+    return charContainer;
 }
 
 
--- Testing.cpp
+++ Testing.cpp
@@ -1,57 +1,85 @@
-// A hello world program of the internet. 
+// A hello world program of the internet.
+
 #include <iostream>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <cstdlib>
 #include <sstream>
-#include <numeric>
+#include <string>
+#include <vector>
+
+
 
 using namespace std;
- 
-// include the ParamReader class.
-#include "CParamReader.h"
-//#include "ParamReader.txt"
 
- 
+
+
+// include the ParamReader class.
+
+#include "CParamReader.h"
+
+
+
 int main()
+
 {
 
-
     // make a param reader object.
+
     CParamReader myReader;
+
     char fileName[] = "Params.txt";
+
     if(! myReader.setNewFileName(fileName))
-	{
-		cout << "File " << fileName << " doesn't exist." << endl; 
-		return 1; 
-	} else
-	{
-		cout << "File exists." << endl; 
-	}
+    {
+        cout << "File " << fileName << " doesn't exist." << endl;
+        return 1;
+    } else
+    {
+        cout << "File exists." << endl;
+    }
 
-    char paramName[] = "vectorTest";
+    
+
+    /*char paramName[] = "demog_eta";
+
     int length;
+
     char* myValue = myReader.getParamString(paramName, length);
-    cout << paramName << " = " << myValue << ": length = " << length << endl;
-	cout << myValue << endl;
 
+    cout << paramName << " = " << myValue << ": length = " << length << endl;*/
 
-	// Lets convert string to vector 
-	string ParamStr=myValue;
-	
-	cout << "The string parameter values: \t\t" << ParamStr << endl;
+    
 
-	stringstream ss;
-	vector<double> data;
-	ss << ParamStr;
-	double a;
-	while (ss >> a)
-    data.push_back(a);
-	
-	cout << " Let's check output of our new vector: \t" << data[0] << " " << data[1] << " " <<data[2] << " " << data[3] << endl;
-	
+    //char v[] = "vectorTest";
+    char ParamName[] = "vectorTest";
+    int length;
 
- 
+    char* myValue = myReader.getParamString(ParamName,length);
+
+    cout << ParamName << " = " << myValue << ": length = " << length << endl;
+    
+    // Lets convert string to vector
+    //string ParamStr;
+    
+    cout << "The string parameter values: \t\t" << myValue << endl;
+    
+    stringstream ss;
+    vector<double> data;
+    ss << myValue;
+    double a;
+    while (ss >> a)
+        data.push_back(a);
+    
+    cout << " Let's check output of our new vector: \t" << data[0] << " " << data[1] << " " <<data[2] << " " << data[3] << endl;
+    
+
+    
+    cout << myValue << endl;
+    
+
     return 0;
+
 }
+
+
+
+
+
